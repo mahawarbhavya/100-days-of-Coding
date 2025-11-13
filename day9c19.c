a9266a32b9aec70af6ea5d570121fd75107d268c
mahawarbhavya <Bhavya.29516@stu.upes.ac.in>
2025-11-13 11:55:11 +0530

    Add triangle type classification program

---
+++ day9c19.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+int main() {
+    int side1, side2, side3;
+
+    printf("Enter the value of side 1\n");
+    scanf("%d", &side1);
+
+    printf("Enter the value of side 2\n");
+    scanf("%d", &side2);
+
+    printf("Enter the value of side 3\n");
+    scanf("%d", &side3);
+
+    if (side1 == side2 && side2 == side3) {
+        printf("The triangle is equilateral\n");
+    } else if (side1 == side2 || side2 == side3 || side3 == side1) {
+        printf("The triangle is isosceles\n");
+    } else {
+        printf("The triangle is scalene\n");
+    }
+
+    return 0;
+}
