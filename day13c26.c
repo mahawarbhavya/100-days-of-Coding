7ba08f5d406b368f8e045aad6d7fa10d625f56f8
mahawarbhavya <Bhavya.29516@stu.upes.ac.in>
2025-11-19 20:35:33 +0530

    Implement loop to print numbers from 1 to n

---
+++ day13c26.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+int main() {
+    int n, i = 1;
+
+    printf("Enter the value of n: ");
+    scanf("%d", &n);
+
+    while (1) {  
+        if (i > n)
+            break; 
+        printf("%d ", i);
+        i++;
+    }
+
+    printf("\n");
+    return 0;
+}
