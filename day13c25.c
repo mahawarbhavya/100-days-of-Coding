116f212065456970d21fd2ae1c1e4fb9098467ec
mahawarbhavya <Bhavya.29516@stu.upes.ac.in>
2025-11-19 20:32:20 +0530

    Implement basic calculator functionality

---
+++ day13c25.c
@@ -0,0 +1,51 @@
+#include <stdio.h>
+
+int main() {
+    float num1, num2, result;
+    char op;
+
+    printf("Enter first number: ");
+    scanf("%f", &num1);
+
+    printf("Enter an operator (+, -, *, /, %%): ");
+    scanf(" %c", &op); 
+    printf("Enter second number: ");
+    scanf("%f", &num2);
+
+    switch(op) {
+        case '+':
+            result = num1 + num2;
+            printf("Result: %.2f\n", result);
+            break;
+        case '-':
+            result = num1 - num2;
+            printf("Result: %.2f\n", result);
+            break;
+        case '*':
+            result = num1 * num2;
+            printf("Result: %.2f\n", result);
+            break;
+        case '/':
+            if (num2 != 0)
+                result = num1 / num2;
+            else {
+                printf("Error: Division by zero.\n");
+                return 1;
+            }
+            printf("Result: %.2f\n", result);
+            break;
+        case '%':
+            if ((int)num2 != 0)
+                result = (int)num1 % (int)num2;
+            else {
+                printf("Error: Modulo by zero.\n");
+                return 1;
+            }
+            printf("Result: %.0f\n", result); 
+            break;
+        default:
+            printf("Invalid operator.\n");
+    }
+
+    return 0;
+}
