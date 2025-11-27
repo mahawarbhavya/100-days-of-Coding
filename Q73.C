12595b126c6eb2969846740ecab4d2f8a7da4260
mahawarbhavya <Bhavya.29516@stu.upes.ac.in>
2025-11-27 16:51:56 +0530

    Create Q73.C

---
+++ Q73.C
@@ -0,0 +1,37 @@
+#include <stdio.h>
+
+int main() {
+    int rows, cols, i, j;
+
+    printf("Enter number of rows: ");
+    scanf("%d", &rows);
+    printf("Enter number of columns: ");
+    scanf("%d", &cols);
+
+    int matrix[rows][cols];
+    int rowSum[rows];
+
+    // Read matrix
+    printf("Enter elements of the matrix:\n");
+    for(i = 0; i < rows; i++) {
+        for(j = 0; j < cols; j++) {
+            scanf("%d", &matrix[i][j]);
+        }
+    }
+
+    // Calculate row sums
+    for(i = 0; i < rows; i++) {
+        rowSum[i] = 0;
+        for(j = 0; j < cols; j++) {
+            rowSum[i] += matrix[i][j];
+        }
+    }
+
+    // Print row sums
+    printf("Row sums stored in array:\n");
+    for(i = 0; i < rows; i++) {
+        printf("%d ", rowSum[i]);
+    }
+
+    return 0;
+}
