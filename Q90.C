873b28a769656ae7a11705d51a1c86ebcb378ebe
mahawarbhavya <Bhavya.29516@stu.upes.ac.in>
2025-11-28 13:17:19 +0530

    Create Q90.C

---
+++ Q90.C
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+int main() {
+    char str[200];
+    printf("Enter a string: ");
+    fgets(str, sizeof(str), stdin);  // reads spaces too
+
+    for(int i=0; str[i] != '\0'; i++) {
+        if(str[i] >= 'a' && str[i] <= 'z') {
+            str[i] = str[i] - 32;  // lowercase → uppercase
+        }
+        else if(str[i] >= 'A' && str[i] <= 'Z') {
+            str[i] = str[i] + 32;  // uppercase → lowercase
+        }
+    }
+
+    printf("Toggled string: %s\n", str);
+
+    return 0;
+}
