a0aa679640e92033a41ab936e0294aeecda3c95e
mahawarbhavya <Bhavya.29516@stu.upes.ac.in>
2025-11-28 13:06:24 +0530

    Create Q85.C

---
+++ Q85.C
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+int main() {
+    char str[100];
+    int i, j, temp;
+
+    printf("Enter a string: ");
+    scanf("%s", str);
+
+    // Find length
+    int length = 0;
+    while(str[length] != '\0') {
+        length++;
+    }
+
+    // Reverse using swapping
+    for(i = 0, j = length - 1; i < j; i++, j--) {
+        temp = str[i];
+        str[i] = str[j];
+        str[j] = temp;
+    }
+
+    printf("Reversed string: %s\n", str);
+
+    return 0;
+}
